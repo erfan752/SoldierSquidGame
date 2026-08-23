e705189dfc1334de0c9ac6741d96858544abab7a
Erf <nabiporerfan476@gmail.com>
2026-08-24 00:15:33 +0330

    بزرگتر کردن سایت

--- fa/js/index.js
+++ fa/js/index.js
@@ -1,13 +1,26 @@
 // بی رنگ کردن هدر زمانی که اسکرول 0 بایشد ====================================
-let header = document.querySelector("header")
+let header = document.querySelector("header");
 function noneBGTo0scroll() {
-    setInterval(() => {
-        if (scrollY === 0) {
-            header.style.background = "rgba(0, 0, 0, 0)";
-        }
-        else {
-            header.style.removeProperty("background")
-        }
-    }, 100)
+  setInterval(() => {
+    if (scrollY === 0) {
+      header.style.background = "rgba(0, 0, 0, 0)";
+      header.style.justifyContent = " center";
+    } else {
+      header.style.removeProperty("background");
+      header.style.justifyContent = "space-between";
+    }
+  }, 100);
 }
-noneBGTo0scroll()
+noneBGTo0scroll();
+
+const lenghtUserNav = document.querySelector("#lenght-user");
+// const
+function animateLenghtUser(lenghtUser) {
+  for (let x = 0; x < lenghtUser + 1; x++) {
+    setTimeout(() => {}, 100);
+    lenghtUserNav.textContent = x;
+    // console.log(x);
+  }
+}
+
+animateLenghtUser(50);
--- fa/js/main.js
+++ fa/js/main.js
@@ -1,85 +1,72 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.166/build/three.module.js";
 
-import {GLTFLoader}
-from "https://cdn.jsdelivr.net/npm/three@0.166/examples/jsm/loaders/GLTFLoader.js";
+import {GLTFLoader} from "https://cdn.jsdelivr.net/npm/three@0.166/examples/jsm/loaders/GLTFLoader.js";
 
-import {OrbitControls}
-from "https://cdn.jsdelivr.net/npm/three@0.166/examples/jsm/controls/OrbitControls.js";
+import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@0.166/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
 scene.background = new THREE.Color(0x050505);
 
 const camera = new THREE.PerspectiveCamera(
-45,
-window.innerWidth/window.innerHeight,
-0.1,
-1000
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
 );
 
-camera.position.set(0,1.5,4);
+camera.position.set(0, 1.5, 4);
 
 const renderer = new THREE.WebGLRenderer({
-antialias:true
+  antialias: true,
 });
 
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.getElementById("scene").appendChild(renderer.domElement);
 
-const light1 = new THREE.DirectionalLight(0xffffff,2);
+const light1 = new THREE.DirectionalLight(0xffffff, 2);
 
-light1.position.set(5,5,5);
+light1.position.set(5, 5, 5);
 
 scene.add(light1);
 
-const red = new THREE.PointLight(0xff0033,30);
+const red = new THREE.PointLight(0xff0033, 30);
 
-red.position.set(0,2,-2);
+red.position.set(0, 2, -2);
 
 scene.add(red);
 
-scene.add(new THREE.AmbientLight(0xffffff,0.5));
-
+scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
 const loader = new GLTFLoader();
 
 let soldier;
 
-loader.load("../assets/models/untitled.glb",(gltf)=>{
+loader.load("../assets/models/untitled.glb", (gltf) => {
+  soldier = gltf.scene;
 
-    soldier = gltf.scene;
+  soldier.position.set(0, -1, 0);
 
-    soldier.position.set(0,-1,0);
+  soldier.scale.set(1.5, 1.5, 1.5);
 
-    soldier.scale.set(1.5,1.5,1.5);
-
-    scene.add(soldier);
-
+  scene.add(soldier);
 });
 
-function animate(){
+function animate() {
+  requestAnimationFrame(animate);
 
-    requestAnimationFrame(animate);
+  controls.update();
 
-    controls.update();
-
-    renderer.render(scene,camera);
-
+  renderer.render(scene, camera);
 }
 
 animate();
 
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
 
-window.addEventListener("resize",()=>{
+  camera.updateProjectionMatrix();
 
-camera.aspect = window.innerWidth/window.innerHeight;
-
-camera.updateProjectionMatrix();
-
-renderer.setSize(
-window.innerWidth,
-window.innerHeight
-);
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
